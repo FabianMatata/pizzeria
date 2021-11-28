1e3279960661ef502de0f23011d3e4da2eb2c635
Fabian Matata <mfabian601@gmail.com>
2021-11-28 19:46:05 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+//Business Logic
+function Address(streetAddress, city, state) {
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.state = state;
+    this.deliveryAddress = (streetAddress + "  " + city + ", " + state);
+}
+
+
+//User Interface Logic
+$(document).ready(function(event) {
+    /////Landing Page Btns
+    $("#pickup-btn").click(function() {
+        $("#order-content").show();
+        $("#landing-content").hide();
+        $("#delivery-option").text("PICKUP BY CUSTOMER");
+    });
+    $("#delivery-btn").click(function() {
+        $("#address").show();
+        $("#pickup-btn,#delivery-btn,#landing-tagline").hide();
+    });
+    $("form#address-form").submit(function(event) {
+        event.preventDefault();
+        var streetAddress = $("input#street-add").val();
+        var city = $("input#city-add").val();
+        var state = $("input#state-select").val();
+        // var zipcode = $("input#zip-add").val();
+        var newAddress = new Address(streetAddress, city, state)
+        $("#order-content").show();
+        $("#landing-content").hide();
+        $("#delivery-option").text("DELIVER TO: " + newAddress.deliveryAddress);
+    });
+    $("#sides-dropdown").click(function() {
+        $("#sides-details").toggle();
+    });
+    ///Checkout Btn
+    $("#checkout-btn").click(function() {
+        location.reload();
+    });
+});
